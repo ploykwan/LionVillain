d0bb0f6a6c1051b5b5707cd4d4f93ebaf8e6a7b6
Kwankaew Uttama <kwankaew.u@ku.th>
2018-04-12 17:20:11 +0700

    home window

--- src/gameUI/MainFrame.java
+++ src/gameUI/MainFrame.java
@@ -1,6 +1,25 @@
 package gameUI;
 
+import javax.swing.JFrame;
+
+/**
+ * Ma
+ * @author kwankaew
+ *
+ */
 public class MainFrame {
 	private final int width = 1280;
 	private final int hight = 720;
+	
+//	public static void main(String[] args) {
+//		HomeUI home = new HomeUI();
+//		
+//		JFrame frame = new JFrame();
+//		
+//		frame.add(home);
+//		frame.setSize(1280,720);
+//		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		frame.setVisible(true);
+//	}
+	
 }
