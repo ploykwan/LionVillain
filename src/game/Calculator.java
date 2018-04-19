11f1409749e9512749cd3e81db25a736c13a1abf
Pimwalun <janny25522009@hitmail.com>
2018-04-19 14:22:56 +0700

    push calculate logic

---
+++ src/game/Calculator.java
@@ -0,0 +1,5 @@
+package game;
+
+public interface Calculator {
+	public int covert(int num1, int num2);
+}
--- src/game/Villager.java
+++ src/game/Villager.java
@@ -2,5 +2,44 @@ package game;
 
 public class Villager {
 	private int distance;
-	private double time1;
+	private double time;
+	private Calculator calculator;
+	private int people;
+	private String name;
+
+	public Villager(String name, double time, int people, int distance) {
+		this.name = name;
+		this.time = time;
+		this.people = people;
+		this.distance = distance;
+	}
+	
+	@Override
+	public String toString() {
+		return name;
+	}
+	
+	public double getTime() {
+		return time;
+	}
+	
+	public void setTime(double time) {
+		this.time = time;
+	}
+	
+	public int getPeople() {
+		return people;
+	}
+	
+	public void setPeople(int people) {
+		this.people = people;
+	}
+	
+	public int getDistance() {
+		return distance;
+	}
+	
+	public void setDistance(int distance) {
+		this.distance = distance;
+	}
 }
