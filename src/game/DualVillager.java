66b71a81bb577c93b4b753204092670b9c3f049b
Pimwalun <janny25522009@hitmail.com>
2018-04-29 16:23:36 +0700

    game logic for 2 player

---
+++ src/game/DualVillager.java
@@ -0,0 +1,38 @@
+package game;
+
+public class DualVillager {
+	private Villager v1, v2;
+	
+	public DualVillager(Villager v1, Villager v2) {
+		this.v1 = v1;
+		this.v2 = v2;
+	}
+	
+	public void V1Correct() {
+		v2.setDistance(v2.getDistance()-v1.getPeople());
+	}
+	
+	public void V2Correct() {
+		v1.setDistance(v1.getDistance()-v2.getPeople());
+	}
+	
+	public boolean V1Lose() {
+		return v1.getDistance() <= 0;
+	}
+	
+	public boolean V2Lose() {
+		return v2.getDistance() <= 0;
+	}
+	
+	public Villager getV1() {
+		return v1;
+	}
+	
+	public Villager getV2() {
+		return v2;
+	}
+	
+	public boolean end() {
+		return V1Lose() || V2Lose();
+	}
+}
