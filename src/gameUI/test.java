da9004c1c8ff6828d047e91ae23fc32ca5d5da0c
Pimwalun <janny25522009@hitmail.com>
2018-05-02 20:25:22 +0700

    test

--- src/gameUI/test.java
+++ src/gameUI/test.java
@@ -1,10 +1,15 @@
 package gameUI;
 
 import java.awt.BorderLayout;
+import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.Toolkit;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -15,68 +20,50 @@ import java.util.Observer;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
-import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JInternalFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.WindowConstants;
 
 import game.Calculator;
 import game.ObjectPool;
 import game.Villager;
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 
 public class test extends JFrame implements Observer {
-	
+
 	int num1 = 0;
 	int num2 = 0;
 	char op;
-	int result,score = 0;
+	int result, score = 0;
 	private String message;
 	private JLabel question;
 	private Calculator game;
 
 	private ObjectPool objectPool;
 	private Renderer renderer;
-	private JPanel panel;
 
 	public test() {
 		game = new Calculator();
 		objectPool = new ObjectPool();
 		objectPool.addObserver(this);
-		
-		panel = new JPanel() {
-			@Override
-			protected void paintComponent(Graphics g) {
-				super.paintComponent(g);
-				try {
-					BufferedImage img = ImageIO.read(this.getClass().getResourceAsStream("/res/single_mode.png"));
-					g.drawImage(img, 0, 0, 1280, 720, null);
-				} catch (IOException e) {
-
-				}
-			}
-		};
-		panel.setBounds(0,0,1280,720);
 
 		renderer = new Renderer();
-		
+		add(renderer);
+
 		question = new JLabel();
 		question.setFont(new Font("Arial Rounded Bold", Font.PLAIN, 40));
 		question.setText("question");
 		question.setBounds(497, 178, 213, 57);
-		renderer.add(question);
-		
+		add(question);
+
 		JTextField textfield = new JTextField();
 		textfield.setFont(new Font("Arial Rounded Bold", Font.PLAIN, 43));
 		textfield.setBounds(710, 168, 105, 75);
-		renderer.add(textfield);
+		add(textfield);
+		question();
+		question.setText(getMessage());
 		textfield.addKeyListener(new KeyListener() {
 			@Override
 			public void keyTyped(KeyEvent e) {
@@ -89,7 +76,6 @@ public class test extends JFrame implements Observer {
 			@Override
 			public void keyPressed(KeyEvent e) {
 				if (e.getKeyCode() == KeyEvent.VK_ENTER) {
-					question.setText(getMessage());
 					int answer = 9999;
 					try {
 						String ans = textfield.getText().trim();
@@ -101,37 +87,34 @@ public class test extends JFrame implements Observer {
 						System.out.println(answer + " ผิด");
 						textfield.setText("");
 					} else { // correct answer
-					System.out.println(e.getKeyCode());
-					try {
+						System.out.println(e.getKeyCode());
 						objectPool.burstVillagers(e.getKeyCode());
-
-					} catch (Exception e2) {
-						e2.getMessage();
+						score++;
+						System.out.println(answer + " ถูก");
+						textfield.setText("");
+						game.setDx(-10); // เพิ่มขึ้นที่ละ x หน่วย
+						game.push();
+						System.out.println(game.getX());
 					}
-					score++;
-					System.out.println(answer + " ถูก");
-					textfield.setText("");
-					game.setDx(10); // เพิ่มขึ้นที่ละ x หน่วย
-					game.push();
-				//	lion.setLocation(game.getX(), 375);
-				 //distance.setText(String.format("%d meter", game.getX() + 20));
-				}
-				if (game.isGameEnd()) {
-//					thread2.stop();
-				//	gameEnd();
-				} else {
-					question();
-					question.setText(getMessage());
+					System.out.println("------------");
+					if (game.isGameEnd()) {
+						System.out.println("***************");
+//						thread.stop();
+//						thread2.stop();
+//						gameEnd();
+					}else {
+						System.out.println("new question");
+						question();
+						question.setText(getMessage());
+					}
 				}
 			}
-				}
 		});
-
 
 		setLayout(new BorderLayout());
 		add(renderer);
 
-		setResizable(false);
+		setResizable(true);
 		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		pack();
 	}
@@ -160,6 +143,14 @@ public class test extends JFrame implements Observer {
 		@Override
 		public void paint(Graphics g) {
 			super.paint(g);
+			
+			BufferedImage img1 = null;
+			try {
+				img1 = ImageIO.read(this.getClass().getResource("/res/single_mode.png"));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			g.drawImage(img1, 0, 0,1280,720, null);
 
 			BufferedImage img = null;
 			try {
@@ -167,11 +158,8 @@ public class test extends JFrame implements Observer {
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-
 			// Draw space
 			for (Villager villager : objectPool.getVillager()) {
-				// g.fillOval(bullet.getX(), bullet.getY(), 100, 100);
-				// System.out.println(villager.getX() + " " + villager.getY());
 				g.drawImage(img, 1200 + villager.getX(), 375 + villager.getY(), 111, 120, null);
 			}
 		}
@@ -182,7 +170,7 @@ public class test extends JFrame implements Observer {
 		char operator[] = { '+', '-', '*', '/' };
 		// TODO ค่อยแก้เลข
 		num1 = (int) (1 + (Math.random() * 1));
-		num2 = (int) (1 + (Math.random() * 1));
+		num2 = (int) (1 + (Math.random() * 10));
 		int id = (int) (Math.random() * 4);
 		op = operator[id];
 		switch (op) {
@@ -215,7 +203,6 @@ public class test extends JFrame implements Observer {
 		setMessage(num1 + " " + op + " " + num2 + " =");
 		System.out.println(num1 + " " + op + " " + num2);
 	}
-	
 
 	public void setMessage(String message) {
 		this.message = message;
@@ -224,22 +211,4 @@ public class test extends JFrame implements Observer {
 	public String getMessage() {
 		return message;
 	}
-	
-	// @Override
-	// public void start(Stage primaryStage) {
-	// Parent root;
-	// try {
-	// root = FXMLLoader.load(getClass().getResource("/gameUI/indexUI.fxml"));
-	// Scene scene = new Scene(root);
-	// primaryStage.setScene(scene);
-	// primaryStage.setTitle("test");
-	// primaryStage.show();
-	// } catch (IOException e) {
-	// e.printStackTrace();
-	// }
-	// }
-	//
-	// public static void main(String[] args) {
-	// launch(args);
-	// }
 }
